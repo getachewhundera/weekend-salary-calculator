a36e9ff0f9fb98a85d583e4b82d2f04ab0eaa207
Getachew Hundera <ghundera1@gmail.com>
2023-07-24 22:02:09 -0500

    appended input to DOM

--- client.js
+++ client.js
@@ -1,6 +1,34 @@
+const { event } = require("jquery");
+
 console.log('salary calculator'); 
 
 
-function onSubmit(event){
-    
+function submitForm(event){
+    console.log('submit form');
+
+    event.preventDefault(); 
+
+    let firstnameInput = document.querySelector('#firstname-input').value 
+    let lastnameIput = document.querySelector('#lastname-input').value 
+    let idInput = document.querySelector('#id-input').value 
+    let jobtitleInput  = document.querySelector('#jobtitle-input').value 
+    let annualsalaryInput = document.querySelector('#annualsalary-input').value 
+
+
+    let employeeData= document.querySelector('#employeetableinfo'); 
+
+
+    employeeData.innerHTML += `
+        <tr>
+            <td>${firstnameInput}</td>
+            <td>${lastnameIput}</td>
+            <td>${idInput}</td>
+            <td>${jobtitleInput}</td>
+            <td>${annualsalaryInput}</td>
+            <td> <button onClick="removeRow(event)">Delete</button> </td>
+        </tr>
+    `;
+
 }
+
+
