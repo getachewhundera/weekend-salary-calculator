aac704190ea08ec04ead79b71287be28e64e17af
Getachew Hundera <ghundera1@gmail.com>
2023-07-28 03:46:38 -0500

    worked on total monthly cost function but is not currently working correctly, added an if statment for changing color when cost is > 20k

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
-// const { event } = require("jquery");
-
 console.log('salary calculator'); 
 
+
+//function to submit form. 
 
 function submitForm(event){
     console.log('submit form');
@@ -12,7 +12,7 @@ function submitForm(event){
     let lastnameIput = document.querySelector('#lastname-input').value 
     let idInput = document.querySelector('#id-input').value 
     let jobtitleInput  = document.querySelector('#jobtitle-input').value 
-    let annualsalaryInput = document.querySelector('#annualsalary-input').value 
+    let annualsalaryInput = document.querySelector('.annualsalary-input').value 
 
 
     let employeeData= document.querySelector('#employeetableinfo'); 
@@ -29,10 +29,47 @@ function submitForm(event){
         </tr>
     `;
 
+    //updates the monthly cost each time an annual salry is inputted. 
+
+    monthlyCost(); 
+
+    
 }
+
+
+//function to calculate the monthly cost 
+
+function monthlyCost(){
+    event.preventDefault(); 
+    let totalannualSalaries = document.querySelectorAll('.annualsalary-input');
+    let totalSalary= 0; 
+
+    for (const salaries of totalannualSalaries){
+         totalSalary += Number(salaries.value);
+    }
+    
+    console.log(totalSalary);
+    
+    let totalMonthlyCost= totalSalary / 12;
+
+    if (totalMonthlyCost > 20000){
+        tMonthlycost.style.backgroundcolor= 'red';  
+    }else {
+        tMonthlycost.style.backgroundcolor = 'initial'; 
+    }
+    
+    let tMonthlycost = document.querySelector('#tMonthlycost');
+    tMonthlycost.innerHTML=`<td> Total Monthly Cost: ${totalMonthlyCost.toFixed(2)} </td>`;
+}
+
+
+
+
+
+
 
 function removeRow(event){
     event.target.closest('tr').remove(); 
+    //updates the monthly cost after an employee's annual salary is deleted. 
+    monthlyCost(); 
 }
-
-
